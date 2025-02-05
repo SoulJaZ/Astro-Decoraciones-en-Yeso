06459371e2fcb2373e647917dfc1ea4fc532fc56
RasSoul <mmagtc1101@gmail.com>
2025-02-05 17:24:05 -0500

    Commit portal decoraciones en yeso

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Decoraciones en Yeso';
+export const SITE_DESCRIPTION = 'Juan Manuel Silva – Maestro de Obra y Artista en Acabados de Construcción!';
